9bb60310285afeed17d7c36dfdda66f3e1743505
ArunKumarGhorse <ghorsearun8@gmail.com>
2026-06-11 18:26:42 +0530

    edit feature added

--- DAY_15/TO_DO_APP/script.js
+++ DAY_15/TO_DO_APP/script.js
@@ -1,16 +1,25 @@
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
+let editIndex = null;
+
 const taskInput = document.getElementById('input');
 const addTaskButton = document.getElementById('addTask');
 const taskList = document.getElementById('task-list');
-const clearButton = document.getElementById('clearTasks');
-const deleteButtons = document.getElementsByClassName('delete');
 
 function renderTasks() {
     taskList.innerHTML = '';
+
     tasks.forEach((task, index) => {
         let li = document.createElement('li');
-        li.innerHTML = `${task} <button onclick="deleteTask(${index})">Delete</button>`;
+
+        li.innerHTML = `
+        <span>${task}</span>
+    <div>
+        <button onclick="editTask(${index})">Edit</button>
+        <button onclick="deleteTask(${index})">Delete</button>
+    </div>
+`;
+
         taskList.appendChild(li);
     });
 }
@@ -18,11 +27,26 @@ function renderTasks() {
 function addTask() {
     const task = taskInput.value.trim();
     if (task === '') return;
-    tasks.push(task);
+
+    if (editIndex !== null) {
+        tasks[editIndex] = task;
+        editIndex = null;
+        addTaskButton.textContent = "Add Task";
+    } else {
+        tasks.push(task);
+    }
+
     localStorage.setItem('tasks', JSON.stringify(tasks));
     taskInput.value = '';
     renderTasks();
 }
+
+function editTask(index) {
+    taskInput.value = tasks[index];
+    editIndex = index;
+    addTaskButton.textContent = "Update Task";
+}
+
 function deleteTask(index) {
     tasks.splice(index, 1);
     localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -34,3 +58,13 @@ function clearTasks() {
     localStorage.removeItem('tasks');
     renderTasks();
 }
+function tg() {
+    document.body.classList.toggle("dark")
+}
+
+renderTasks();
+
+window.editTask = editTask;
+window.deleteTask = deleteTask;
+window.addTask = addTask;
+window.clearTasks = clearTasks;
--- Day_18_String_Fun/toLowercase_toUpperCase.js
+++ Day_18_String_Fun/toLowercase_toUpperCase.js
@@ -1,6 +1,10 @@
-const name = "arun";
-console.log(name.toUpperCase()); // ARUN
+// const name = "arun";
+// console.log(name.toUpperCase()); // ARUN
 
-let arr = ["arun", "Om", "aman"];
-let res = arr.map(name => name.toUpperCase());
-console.log(res); // ["ARUN", "RAHUL", "AMAN"]
+// let arr = ["arun", "Om", "aman"];
+// let res = arr.map(name => name.toUpperCase());
+// console.log(res); // ["ARUN", "RAHUL", "AMAN"]
+
+// let arr = ["ARUN", "rahul", "AMAN"];
+// let res = arr.filter(word => word === word.toUpperCase());
+// console.log(res); // ["ARUN", "AMAN"]
